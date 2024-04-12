8f4583aaf68086ddeecc9ba5b862d87530e24e11
Jose Anneudy Eustate Nunez <aneudyen@Joses-Air.local.tld>
2024-04-12 11:16:28 -0400

    Passed props to Post component

--- social-media-app/src/components/Post.jsx
+++ social-media-app/src/components/Post.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import react, {useState} from 'react';
 import Comment from './Comment'
-function Post(){
+function Post(props){
+
+    const[likes, setLikes]= useState(0);
+
+    function handleLike(){
+        setLikes(likes + 1);
+    }
     return(
         <div>
         <h3>Test Post</h3>
         <p>This is a test post we'll edit later!</p>
         <p>Likes: 30</p>
+        <button>Like</button>
         <p>Comments:</p>
         <Comment />
+        <Comment content="This is a test comment!" />
+        <Comment content="This is another test comment!" />
+        <Comment content="This is yet another test comment!" />
         </div>
     )
 }

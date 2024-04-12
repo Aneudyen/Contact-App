5361fb39797edb3e9c75431b46a064b8eada5463
Jose Anneudy Eustate Nunez <aneudyen@Joses-Air.local.tld>
2024-04-12 18:09:59 -0400

    Creating acontact.jsx file

--- social-media-app/src/main.jsx
+++ social-media-app/src/main.jsx
@@ -8,10 +8,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+import ErrorPage from "./error-page";
+
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />
+    element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
